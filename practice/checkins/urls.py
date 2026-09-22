c3b5f41bcef63e278cc0e25c7d8feba6be4862ba
Yeganeh Khayyat <Yeganehkhayyat.1@gmail.com>
2026-09-22 07:27:16 +0330

    fix: bug fix

--- practice/checkins/urls.py
+++ practice/checkins/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-
+from django.conf import settings
+from django.conf.urls.static import static
 from checkins.views import about, get_form, home, report
 
 app_name = "checkins"
@@ -10,4 +11,4 @@ urlpatterns = [
     path("add/", get_form, name="add"),
     path("report/", report, name="report"),
     path("about/", about, name="about"),
-]
+] + static(settings.STATIC_URL, document_root = settings.STATIC_URL)
