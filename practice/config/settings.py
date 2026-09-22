2f2e83adf5b978f00456b24577c23295ee85a887
Yeganeh Khayyat <Yeganehkhayyat.1@gmail.com>
2026-09-22 06:43:19 +0330

    fix: bug fix

--- practice/config/settings.py
+++ practice/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-l%p--u2kk^j0np5c*upz50hbfz+m+m*qnah&5#d8zhb4n_lzp!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["yeganehkhayyat.pythonanywhere.com"]
 
 
 # Application definition
